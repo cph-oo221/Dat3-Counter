145f4d43eb4b34b381b179fe83a3b4a2c0ba46ea
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-11-20 16:47:49 +0100

    Update Timer.jsx

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -5,8 +5,24 @@ function Timer() {
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [timerOn, setTimerOn] = useState(false);
+  const [pausedTime, setPausedTime] = useState(0);
+
+  let pauseText = timerOn ? "Pause" : "Start";
 
   function startTimer() {
+    if (timerOn) {
+      setPausedTime(time);
+      setTimerOn(false);
+      return;
+    }
+
+    if (pausedTime > 0) {
+      setTime(pausedTime);
+      setPausedTime(0);
+      setTimerOn(true);
+      return;
+    }
+
     setTimerOn(true);
     setTime(minutes * 60 + seconds);
   }
@@ -16,6 +32,15 @@ function Timer() {
     setTime(0);
     setSeconds(0);
     setMinutes(0);
+    setPausedTime(0);
+  }
+
+  function setPresetTime(presetMinutes) {
+    resetTimer();
+    setMinutes(presetMinutes);
+    setSeconds(0);
+    setTime(presetMinutes * 60);
+    setTimerOn(true);
   }
 
   function formatTime(time) {
@@ -65,8 +90,14 @@ function Timer() {
       </div>
 
       <div>
-        <button onClick={startTimer}>Start</button>
-        <button onClick={resetTimer}>Reset</button>
+        <button onClick={() => setPresetTime(5)}>5 mins</button>
+        <button onClick={() => setPresetTime(10)}>10 mins</button>
+        <button onClick={() => setPresetTime(15)}>15 mins</button>
+      </div>
+
+      <div>
+        <button onClick={startTimer}>{pauseText}</button>
+        <button onClick={resetTimer}>Stop</button>
       </div>
     </div>
   );
